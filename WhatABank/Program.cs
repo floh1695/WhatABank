7eed5697d48b81cda4e3f8fd9fe22c708014cf20
Bayley <cbayley@proforma.com>
2018-04-07 13:33:42 -0400

    Removed bad user/account/storage files * Cleaned up some of my prompt code

--- WhatABank/Program.cs
+++ WhatABank/Program.cs
@@ -10,13 +10,60 @@ namespace WhatABank
     {
         static void Main(string[] args)
         {
-            var user = new User(Console.ReadLine());
+            var loginPrompt = new Prompt();
 
-            var prompt = new Prompt();
-            prompt.RegisterCommand(new Command("help", "this help message", null, Special.HELP));
-            prompt.RegisterCommand(new Command("exit", "exit the program", () => { Console.WriteLine("Goodbye!"); }, Special.EXIT));
+            loginPrompt.RegisterCommand(new Command("login", "login your user",
+                () =>
+                {
+                    Console.Write("username: ");
+                    var username = Console.ReadLine();
 
-            prompt.Run();
+                    Console.Write("password: ");
+                    var password = MaskedInput();
+
+                    var accountPrompt = new Prompt();
+                    accountPrompt.RegisterCommand(new Command("set account", "set account data",
+                        () =>
+                        {
+                            
+                        }));
+
+                    accountPrompt.Run();
+                }));
+
+            loginPrompt.Run();
+
+            Console.WriteLine("Goodbye!");
+            Console.ReadLine();
+        }
+
+        static string MaskedInput()
+        {
+            var input = "";
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+                else if (keyInfo.Key == ConsoleKey.Backspace)
+                {
+                    if (input.Length > 0)
+                    {
+                        input = input.Remove(input.Length - 1);
+                    }
+                    Console.Write("\b \b");
+                }
+                else
+                {
+                    input += keyInfo.KeyChar;
+                    Console.Write('*');
+                }
+            }
+            Console.WriteLine();
+            return input;
         }
     }
+
 }
--- WhatABank/Prompt.cs
+++ WhatABank/Prompt.cs
@@ -20,12 +20,24 @@ namespace WhatABank
             {
                 Console.Write(PromptStyle);
                 var input = Console.ReadLine();
+
+                var helpCommand = new Command("help", "this help message", null, Special.HELP);
+                var exitCommand = new Command("exit", "exit the program", null, Special.EXIT);
                 var errorCommand = new Command(() => Console.WriteLine($"'{input}' is not a valid command"));
-                var command = Commands.FirstOrDefault((cmd) => input == cmd.TextCommand) ?? errorCommand;
+
+                Command command;
+                if (input == "help") { command = helpCommand; }
+                else if (input == "exit") { command = exitCommand; }
+                else
+                {
+                    command = Commands
+                        .FirstOrDefault((cmd) => input == cmd.TextCommand)
+                        ?? errorCommand;
+                }
+
                 command.Callback();
                 if (command.SpecialOption == Special.EXIT)
                 {
-                    Console.ReadLine();
                     break;
                 }
                 else if (command.SpecialOption == Special.HELP)
@@ -37,10 +49,12 @@ namespace WhatABank
 
         private void HelpMessage()
         {
+            Console.WriteLine("help : this help message");
             foreach (var command in Commands.Where(cmd => cmd.TextCommand != ""))
             {
                 Console.WriteLine($"{command.TextCommand} : {command.Help}");
             }
+            Console.WriteLine("exit : exit the prompt");
         }
 
         public void RegisterCommand(Command command)
